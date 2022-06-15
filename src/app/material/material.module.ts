16816e154716bb11b782e146e5f2fc981aace9fc
danielpalli <dpallicolque@est.frba.utn.edu.ar>
2022-06-15 00:45:58 -0300

    Agregado modulos de auth y dashboard

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  exports: [],
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class MaterialModule { }
