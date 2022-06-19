04a6eaed091f06ff272a1f290d687ab1ea024a2e
danielpalli <dpallicolque@est.frba.utn.edu.ar>
2022-06-19 11:06:00 -0300

    Update formulario de registro

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  btnColour = '#a54f4f';
+
+  registroForm: FormGroup = this.fb.group({
+    nombre: [''],
+    apellido: [''],
+    direccion: [''],
+    telefono: [''],
+    perfil: [''],
+    email: [''],
+    confirmarEmail: [''],
+    password: [''],
+    confirmarPassword: [''],
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  registrarse(){
+    console.log("hola");
+  }
 }
