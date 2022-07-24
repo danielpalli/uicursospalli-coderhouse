97b2a7a190d8500ba0ebaa9817996d21c3e21ee9
Daniel Palli <feerhh91@gmail.com>
2022-07-23 23:11:56 -0300

    Update

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -11,15 +11,20 @@ export class ToolbarComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
   respuesta: boolean = false;
   titulo: string = 'Alumnos';
-
-
+  get usuario() {
+    return this.authService.usuario;
+  }
   ngOnInit(): void {
     if (this.authService.role === 'Admin') {
       this.respuesta = true;
     }
+    this.obtenerNombreUbicacion();
+  }
+
+  obtenerNombreUbicacion() {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.titulo =  event.url.split('/')[2];
+        this.titulo = event.url.split('/')[2];
         if (this.titulo === 'alumnos') {
           this.titulo = 'Alumnos';
         }
