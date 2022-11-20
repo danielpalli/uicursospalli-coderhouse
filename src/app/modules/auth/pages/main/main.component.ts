8daf4a119fc74d87bce1a1a0cacbc5da87c20b6d
Daniel Palli <56980051+danielpalli@users.noreply.github.com>
2022-11-20 14:03:24 -0300

    Update main component

--- src/app/modules/auth/pages/main/main.component.ts
+++ src/app/modules/auth/pages/main/main.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { routingAnimation } from 'src/app/modules/route-animations';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [routingAnimation],
 })
 export class MainComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
-
 }
