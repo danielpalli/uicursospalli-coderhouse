4a846e7ca58487108ddf61ca8fe699a5c873dd6b
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2023-12-30 18:57:54 -0300

    Auto-generated commit [ci skip]

---
+++ browser/chunk-G5X6LOVN.js
@@ -0,0 +1,1 @@
+import{Ca as r,Fa as s,T as o,Wa as m,xa as i,ya as a}from"./chunk-IGJNJ4IL.js";var l=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-ui"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,d){n&1&&(i(0,"p"),r(1,"ui works!"),a())},dependencies:[m]});let t=e;return t})();export{l as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-D6RJQJ5O.mjs";import"./chunk-55T3WRTX.mjs";import"./chunk-W6VD74MR.mjs";import"./chunk-YR4H5GCM.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
