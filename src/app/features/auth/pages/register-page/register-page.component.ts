93e3c0d25431ed33658d4ab3f5569f890a89ecc5
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2023-12-30 21:22:08 -0300

    Update login

--- src/app/features/auth/pages/register-page/register-page.component.ts
+++ src/app/features/auth/pages/register-page/register-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
 })
 export default class RegisterPageComponent {
-
+  signUp() {
+    console.log('Registrando...');
+  }
 }
