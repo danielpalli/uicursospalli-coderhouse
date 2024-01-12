6399de2189b2881fb4dd7227ab480712a8117305
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2024-01-12 19:13:23 -0300

    cards add

---
+++ src/app/features/ui/components/card-cursos/card-cursos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ui-card-cursos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-cursos.component.html',
+  styleUrl: './card-cursos.component.css'
+})
+export class CardCursosComponent {
+
+}
--- src/app/features/ui/pages/course-enrollment-page/course-enrollment-page.component.ts
+++ src/app/features/ui/pages/course-enrollment-page/course-enrollment-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardCursosComponent } from '../../components/card-cursos/card-cursos.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardCursosComponent],
   templateUrl: './course-enrollment-page.component.html',
   styleUrl: './course-enrollment-page.component.css'
 })
